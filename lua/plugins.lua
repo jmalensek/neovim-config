c4a666b61d07f44668f1bd58f69d619f538383a5
jmalensek <jm69569@student.uni-lj.si>
2024-03-18 11:27:50 +0100

    Removed null-ls, as it is deprecated and does not function properly

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -58,8 +58,6 @@ return packer.startup(function(use)
 
 	use('williamboman/mason-lspconfig.nvim')
 
-	use('jose-elias-alvarez/null-ls.nvim') -- for formatters and linters
-
 	use('hrsh7th/nvim-cmp')
 
 	use('hrsh7th/cmp-nvim-lsp')
